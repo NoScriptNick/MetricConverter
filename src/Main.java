daad24f029c1e2b6a68ba1c78651fdda1e71bd5b
613668 <beres.613668@swl.k12.oh.us>
2023-10-11 09:28:05 -0400

    sdgsd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double meter = 0;
+        boolean done = false;
+        final double METERS_TO_MILES = 0.00062137;
+        final double METERS_TO_FEET = 3.28084;
+        final double METERS_TO_INCHES = 39.37008;
+
+        //Loops until user enters valid meter measurement
+        do {
+            System.out.println("Enter a meter measurement: ");
+            if (scan.hasNextDouble()) {
+                meter = scan.nextDouble();
+                if (meter < 0) {
+                    System.out.println("Invalid meter measurement");
+                    scan.nextLine();
+                } else {
+                    done = true;
+                }
+            } else {
+                System.out.println("Invalid meter measurement");
+                scan.nextLine();
+            }
+        } while (!done);
+
+        //convert meters to miles, feet, and inches then prints
+        System.out.printf("%-10s %.2f","Meters:",meter);
+        System.out.printf("\n%-10s %.2f","Miles:",meter*METERS_TO_MILES);
+        System.out.printf("\n%-10s %.2f","Feet:",meter*METERS_TO_FEET);
+        System.out.printf("\n%-10s %.2f","Inches:",meter*METERS_TO_INCHES);
+
     }
 }
